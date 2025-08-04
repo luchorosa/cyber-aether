74c54e626d7a417753ae624d4a32cc45cfe2b0f9
Luciano Rosa <14005368+luchorosa@users.noreply.github.com>
2025-08-04 18:48:33 -0300

    Added text format to the RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,21 @@
-import rss from '@astrojs/rss';
+import rss, { type RSSFeedItem } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import type { APIContext } from 'astro';
+import { marked } from 'marked';
 
 export async function GET(context: APIContext) {
   const posts = await getCollection("posts");
 
-  const items = posts
-    .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
-    .map((post) => ({
-      title: post.data.title,
-      pubDate: new Date(post.data.date),
-      link: `/blog/${post.slug}/`,
-      content: post.body,
-    }));
+  const items: RSSFeedItem[] = await Promise.all(
+    posts
+      .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
+      .map(async (post): Promise<RSSFeedItem> => ({
+        title: post.data.title,
+        pubDate: new Date(post.data.date),
+        link: `/blog/${post.slug}/`,
+        content: await marked(post.body), // transforma el markdown en HTML para el RSS
+      }))
+  );
 
   return rss({
     title: "Cyber Aether",
