01cd2dcb52728681e9d31fb6d6b558bc5dfee69b
Luciano Rosa <14005368+luchorosa@users.noreply.github.com>
2025-08-04 17:55:17 -0300

    Added TailwindCSS safelist

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,9 @@ export default defineConfig({
   site: 'https://cyberaether.xyz',
   integrations: [tailwind()],
   outDir: './dist',
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx,svelte,vue}'],
+  safelist: [
+    'border-b-[#4F316F]',
+    'bg-[#24093F]'
+  ],
 });
